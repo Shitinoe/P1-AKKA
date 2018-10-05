7b128e426556c1d1f86bf3029a469e87aeb52d4b
Felipe Shitinoe <felipeshitinoesantos@gmail.com>
2018-10-05 20:18:36 +0000

    Third commit

--- src/main/scala/mainProject.scala
+++ src/main/scala/mainProject.scala
@@ -9,12 +9,13 @@ import java.io.File;
 
 object leitor extends App{
     var matrizA = Array.ofDim[Int](1000,1000)
+    var matrizB = Array.ofDim[Int](1000,1000)
     
     case object EscravoComeceLeitura
     
-    class Escravo extends Actor{
+    class EscravoA extends Actor{
         def receive = {
-            case EscravoComeceLeitura => //escravo1 le o arquivo ppar
+            case EscravoComeceLeitura => //escravoA le o arquivo ppar
                 val sc = new Scanner(new File("src/main/scala/ppar"))
                 var x, y = 0
                 while(sc.hasNextLine()){
@@ -27,13 +28,37 @@ object leitor extends App{
                     x = 0
                     y = y + 1
                 }
+                Thread.sleep(2000)
+                sc.close
+                sistema.shutdown
+        }
+    }
+    
+    class EscravoB extends Actor{
+        def receive = {
+            case EscravoComeceLeitura => //escravoB le o arquivo ppar
+                val sc = new Scanner(new File("src/main/scala/ppar2"))
+                var x, y = 0
+                while(sc.hasNextLine()){
+                    var linha = sc.nextLine()
+                    var valores = linha.split(" ")
+                    while(x < 1000){
+                        matrizB(x)(y) = valores(x).toInt
+                        x = x + 1
+                    }
+                    x = 0
+                    y = y + 1
+                }
+                Thread.sleep(200)
                 sc.close
                 sistema.shutdown
         }
     }
     
     val sistema = ActorSystem("Sistema")
-    val escravo = sistema.actorOf(Props[Escravo], "escravo")
+    val escravoA = sistema.actorOf(Props[EscravoA], "escravoA")
+    val escravoB = sistema.actorOf(Props[EscravoB], "escravoB")
     
-    escravo ! EscravoComeceLeitura
+    escravoA ! EscravoComeceLeitura
+    escravoB ! EscravoComeceLeitura
 }
