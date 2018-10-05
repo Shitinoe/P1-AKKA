e537f807e7a0c7a7c611530650df6dde83841548
Felipe Shitinoe <felipeshitinoesantos@gmail.com>
2018-10-05 17:58:24 +0000

    Second commit

--- src/main/scala/mainProject.scala
+++ src/main/scala/mainProject.scala
@@ -4,34 +4,31 @@ import scala.io.Source
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
-
+import java.util.Scanner
+import java.io.File;
 
 object leitor extends App{
-    val arquivo = Source.fromFile("src/main/scala/ppar")
     var matrizA = Array.ofDim[Int](1000,1000)
     
     case object EscravoComeceLeitura
     
     class Escravo extends Actor{
         def receive = {
-            case EscravoComeceLeitura =>
-                println("EscravoA começou a calcular")
-                var coluna = 0
-                var linha = 0
-                var linhaStr = arquivo.getLines().next.mkString.split(" ")
-                
-                for(line <- arquivo.getLines){
-                    while(coluna < 1000){
-                        matrizA(coluna)(linha) = linhaStr(coluna).toInt
-                        coluna = coluna + 1
+            case EscravoComeceLeitura => //escravo1 le o arquivo ppar
+                val sc = new Scanner(new File("src/main/scala/ppar"))
+                var x, y = 0
+                while(sc.hasNextLine()){
+                    var linha = sc.nextLine()
+                    var valores = linha.split(" ")
+                    while(x < 1000){
+                        matrizA(x)(y) = valores(x).toInt
+                        x = x + 1
                     }
+                    x = 0
+                    y = y + 1
                 }
-                println(matrizA(999)(0).toString())
-                println(matrizA(0)(1).toString())
-                println(matrizA(999)(999).toString())
-                Thread.sleep(5000)
+                sc.close
                 sistema.shutdown
-                arquivo.close
         }
     }
     
@@ -40,9 +37,3 @@ object leitor extends App{
     
     escravo ! EscravoComeceLeitura
 }
-
-//object leitor extends Apps{
-//    def main(args: Array[String]) : Unit {
-//        println("Estou no método main")
-//    }
-//}
